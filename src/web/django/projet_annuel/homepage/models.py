13994d75c3e4bca03c5469b9bd7eb127e46be001
Cecile Cousin <cousincecilem@gmail.com>
2020-05-14 14:41:28 +0200

    add css

--- src/web/django/projet_annuel/homepage/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Recherche(models.Model):
-    first_name = models.CharField(max_length=30)
-    photo = models.FileField(upload_to='candidate-photos')
