cfba436420532a5b16f24a42ea57f03b2b208b3e
Tavernier Antoine <tavernier.antoine@hotmail.fr>
2020-05-18 17:16:36 +0200

    ml

--- src/scraping/model.py
+++ src/scraping/model.py
@@ -1,8 +1,32 @@
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import os
+import tensorflow as tf
+
+def build_model(shape):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(64, activation='relu', input_shape=[shape]),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
+
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+
+    model.compile(loss='mse',
+                optimizer=optimizer,
+                metrics=['mae', 'mse'])
+    return model
 
 script_dir = os.path.dirname(__file__)
 data = pd.read_csv(os.path.join(script_dir,"..\\datasets\\lbc_75_append.csv"))
@@ -10,7 +34,6 @@ data = pd.read_csv(os.path.join(script_dir,"..\\datasets\\lbc_75_append.csv"))
 y = data[["valeur_fonciere"]]
 X = data[["code_postal", "type_local", "surface", "nombre_pieces_principales", "month", "year"]]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+model = build_model(len(X.keys()))
 
-print(X_train)
-print(y_train)
+model.fit(x=X, y=y, epochs=100, batch_size=1000, verbose=1, validation_split=0.20)
