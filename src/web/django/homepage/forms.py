3cf5b33953c63a2f6341fbbf61cc125d9babea21
Arastal <bergeron.florian@gmail.com>
2020-05-27 13:32:15 +0200

    [UP] - update requirements libraries & openstreetmap padding problem

--- src/web/django/homepage/forms.py
+++ src/web/django/homepage/forms.py
@@ -4,7 +4,10 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
 
 class SearchForm(forms.Form):
-    departement = forms.IntegerField(label='Département', min_value=75001, max_value=75020) 
+    departement = forms.IntegerField(label='Département', min_value=75001, max_value=75020)
+
+    price = forms.IntegerField(label='Prix', min_value=0, max_value=1000000)
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -14,5 +17,6 @@ class SearchForm(forms.Form):
 
         self.helper.layout = Layout(
             'departement',
+            'price',
             Submit('submit', 'Submit', css_class='btn-success')
         )
