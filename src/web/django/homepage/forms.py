e4baf900ca872662155d7afc7ca210ccdcd41d14
Cecile Cousin <cousincecilem@gmail.com>
2020-05-27 14:01:27 +0200

    slider ok

--- src/web/django/homepage/forms.py
+++ src/web/django/homepage/forms.py
@@ -2,9 +2,14 @@ from django import forms
 from .models import Search
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit
+from django_range_slider.fields import RangeSliderField
 
 class SearchForm(forms.Form):
-    departement = forms.IntegerField(label='Département', min_value=75001, max_value=75020) 
+    CHOICES = [('1', 'Maison'), ('2', 'Appartement')]
+    departement = forms.IntegerField(label='Département', min_value=75001, max_value=75020)
+    type_local = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
+    superficie = RangeSliderField(label="Superficie(m²):",minimum=5,maximum=1000)
+    nb_pieces = RangeSliderField(label="Nombre de pieces:",minimum=1,maximum=10)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -14,5 +19,9 @@ class SearchForm(forms.Form):
 
         self.helper.layout = Layout(
             'departement',
+            'type_local',
+            'superficie',
+            'nb_pieces',
             Submit('submit', 'Submit', css_class='btn-success')
+
         )
