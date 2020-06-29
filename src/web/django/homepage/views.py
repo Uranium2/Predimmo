2b9b2c1eea6b0828a2757782dde8c54b8cb9af4e
Uranium2 <tavernier.antoine@hotmail.fr>
2020-06-29 17:19:30 +0200

    [UP] Print annonces in index + fix annonces posts

--- src/web/django/homepage/views.py
+++ src/web/django/homepage/views.py
@@ -4,9 +4,10 @@ from .forms import AnnonceForm
 from .forms import SearchForm, PredictionForm, default_predictionForm, default_searchForm
 import requests
 import json
-from mysqlcon import get_conn
+from mysqlcon import get_conn, create_query_search
 import os
 from django.shortcuts import redirect
+from datetime import date
 
 
 def template_color(stroke_color, fille_color):
@@ -43,23 +44,22 @@ def make_map():
 
 def index(request):
     result = ""
+    annonces = list()
     if request.method == 'POST':
         formSearch = SearchForm(request.POST)
         formPrediction = PredictionForm(request.POST)
 
         if formSearch.is_valid():
-            departement = formSearch.cleaned_data['departement']
-            price = formSearch.cleaned_data['price']
-            price_convert = str(price)
             try:
                 conn = get_conn()
                 with conn.cursor() as cursor:
-                    sql = "SELECT * FROM data_django WHERE price = " + price_convert + ""
+                    sql = create_query_search(formSearch)
                     print(sql)
                     cursor.execute(sql)
                     result = cursor.fetchall()
                     conn.commit()
                     print(result)
+                    annonces = list(result)
             finally:
                 conn.close()
 
@@ -67,8 +67,6 @@ def index(request):
         elif formPrediction.is_valid():
             print("prediction ok")
             formSearch = default_searchForm()
-
-
     else:
         # Valeurs par défaut
         formSearch = default_searchForm()
@@ -77,8 +75,9 @@ def index(request):
     coords, styles = make_map()
 
     forms = [formSearch, formPrediction]
+    print(annonces)
 
-    return render(request, 'index.html', {'forms': forms, 'coords': coords, 'styles': styles})
+    return render(request, 'index.html', {'forms': forms, 'coords': coords, 'styles': styles, 'annonces': annonces})
 
 
 def annonce(request):
@@ -90,7 +89,7 @@ def annonce(request):
         adresse = form.data['adresse']
         code_postal = form.data['code_postal']
         url = str(("http://api-adresse.data.gouv.fr/search/?q=" + str(adresse) + "&postcode=" + str(code_postal)))
-
+        print(url)
         r = requests.get(url, headers=headers, data="")
         js = json.loads(r.text)
         x = js['features'][0]['geometry']['coordinates']
@@ -102,22 +101,32 @@ def annonce(request):
         print(x)
 
         if form.is_valid():
+            print("form valid")
             conn = get_conn()
             try:
                 with conn.cursor() as cursor:
                     # Create a new record
-
-                    sql = "INSERT INTO `data_django` (`valeur_fonciere`, `code_type_local`,`type_local`, `surface_reelle_bati`, `nombre_pieces_principales`,`surface_terrain`,`longitude`,`latitude`,`message`,`price`) VALUES (%s, %s,%s, %s,%s, %s,%s,%s,%s, %s)"
+                    date_mutation = date.today().strftime("%y-%m-%d")
+                    print(date_mutation)
+                    print(x)
+                    sql = "INSERT INTO data_django (`date_mutation`, `code_postal`, `valeur_fonciere`, `code_type_local`, `surface_reelle_bati`, `nombre_pieces_principales`,`surface_terrain`,`longitude`,`latitude`,`message`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
                     cursor.execute(sql, (
-                        form.data['price'], form.data['code_postal'], form.data['type_local'],
+                        date_mutation,
+                        form.data['code_postal'],
+                        form.data['price'],
+                        form.data['type_local'],
                         form.data['surface_reelle_bati'],
-                        form.data['nombre_pieces_principales'], form.data['surface_terrain'], form.data['message'],
-                        longitude, latitude, form.data['price']))
+                        form.data['nombre_pieces_principales'],
+                        form.data['surface_terrain'],
+                        longitude,
+                        latitude,
+                        form.data['message']))
                     conn.commit()
                     print("inséré")
             finally:
                 conn.close()
                 return redirect('/index/')
     else:
+        print("form not valid")
         form = AnnonceForm()
     return render(request, "annonce.html", {'form': form, 'coords': coords, 'styles': styles})
--- src/web/django/mysqlcon.py
+++ src/web/django/mysqlcon.py
@@ -7,3 +7,23 @@ def get_conn():
         user='admin',
         password='N8XR3u#m9[5Mk6UK',
         port=3306)
+
+def create_query_search(formSearch):
+    departement = formSearch.cleaned_data['departement']
+    price = formSearch.cleaned_data['price']
+    type_local = formSearch.cleaned_data['type_local']
+    superficie = int(formSearch.cleaned_data['superficie'])
+    nb_pieces = int(formSearch.cleaned_data['nb_pieces'])
+    price_low = price - 10 / 100 * price
+    price_up = price + 10 / 100 * price
+    superficie_low = superficie - 10 / 100 * superficie
+    superficie_up = superficie + 10 / 100 * superficie
+    nb_pieces_low = nb_pieces - 1
+    nb_pieces_up = nb_pieces + 1
+    return "select * from data_django"
+    # return "SELECT * FROM data_django WHERE valeur_fonciere BETWEEN " + str(price_low) + " AND " + str(price_up) + \
+    #             " AND code_type_local = " + str(type_local) + \
+    #             " AND surface_terrain BETWEEN " + str(superficie_low) + " AND " + str(superficie_up) + \
+    #             " AND nombre_pieces_principales BETWEEN " + str(nb_pieces_low) + " AND " + str(nb_pieces_up) + \
+    #             " AND code_postal = " + str(departement)
+                                    
